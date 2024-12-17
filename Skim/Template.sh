3fdc6246e946df1fd257359e95cb7f877cc4fb6a
Eliott <ducarme.eliott@gmail.com>
2024-12-17 16:58:12 +0100

    run on the cluster tmpdir

--- Skim/Template.sh
+++ Skim/Template.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
+
 echo "Hello World, I run on the cluster !"
+echo
+echo "Transfer file to $TMPDIR and go to the dir"
+rsync -aP $1 $TMPDIR/input.root
+cd $TMPDIR
+echo "Done !"
+echo
+echo "load LCG env"
+source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_106a x86_64-el9-gcc14-opt
+echo "Done !"
+echo
+echo "Compile skim.cxx"
+g++ -o skim ~/Run3/Skim/skim.cxx  $(root-config --cflags --libs)
+echo "Done !"
+echo
+echo "Run the skimming procedure"
+./skim $TMPDIR/input.root
+echo "Done !"
+echo
+echo "Transfering to /pnfs"
+rsync -aP output.root $2
+echo "Finished !"
